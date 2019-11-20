4639c208a17133567bdb81d4286be3b2042def26
Ahsan Saleem <ahsansaleemreal@gmail.com>
2019-11-20 17:29:40 +0500

    impliment moveable boderless interface/template

--- TimeTableGenerator/TimeTableGenerator/Template/templateForm.Designer.cs
+++ TimeTableGenerator/TimeTableGenerator/Template/templateForm.Designer.cs
@@ -28,23 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(templateForm));
+            this.topPanelMover = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // topPanelMover
+            // 
+            this.topPanelMover.BackColor = System.Drawing.Color.Maroon;
+            this.topPanelMover.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topPanelMover.Location = new System.Drawing.Point(0, 0);
+            this.topPanelMover.Name = "topPanelMover";
+            this.topPanelMover.Size = new System.Drawing.Size(1400, 50);
+            this.topPanelMover.TabIndex = 0;
+            this.topPanelMover.MouseDown += new System.Windows.Forms.MouseEventHandler(this.topPanelMover_MouseDown);
+            this.topPanelMover.MouseMove += new System.Windows.Forms.MouseEventHandler(this.topPanelMover_MouseMove);
+            this.topPanelMover.MouseUp += new System.Windows.Forms.MouseEventHandler(this.topPanelMover_MouseUp);
             // 
             // templateForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.ClientSize = new System.Drawing.Size(1386, 629);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1400, 630);
+            this.Controls.Add(this.topPanelMover);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "templateForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "templateForm";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel topPanelMover;
     }
 }
--- TimeTableGenerator/TimeTableGenerator/Template/templateForm.cs
+++ TimeTableGenerator/TimeTableGenerator/Template/templateForm.cs
@@ -16,5 +16,27 @@ namespace TimeTableGenerator.Template
         {
             InitializeComponent();
         }
+        private int mouseX = 0;
+        private int mouseY= 0;
+        private bool mouseDown;
+        private void topPanelMover_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            mouseX = e.X;
+            mouseY = e.Y;
+        }
+
+        private void topPanelMover_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(mouseDown)
+            {
+                this.SetDesktopLocation(MousePosition.X-mouseX, MousePosition.Y-mouseY);
+            }
+        }
+
+        private void topPanelMover_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
     }
 }
